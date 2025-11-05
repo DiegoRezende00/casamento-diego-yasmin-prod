e59c58fd86de1c0d7610f000c0b0b4fe689e801c
Diego Rezende <fdcrezende@gmail.com>
2025-11-05 00:05:47 -0300

    Alterando a sequencia do header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
 import Home from "./pages/Home";
 import Casamento from "./pages/Casamento";
 import Presentes from "./pages/Presentes";
