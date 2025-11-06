178fa65ed370b20c51215cc140f21dc3de0ed2d0
Diego Rezende <fdcrezende@gmail.com>
2025-11-06 14:17:11 -0300

    Ajustando a index.html e o vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // 🔥 essencial para funcionar no Vercel
+  root: "src", // 👈 define a raiz do projeto como src
+  base: "./",
   build: {
-    outDir: "dist", // garante que o build vai pra pasta certa
+    outDir: "../dist", // 👈 dist vai ser gerado fora da pasta src
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
   },
 });
